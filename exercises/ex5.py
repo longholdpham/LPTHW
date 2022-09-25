bb0d1b99e5c7a21ff24947816dc3f8b0e3d43263
longholdpham <longpham9903@gmail.com>
2022-09-24 20:12:31 -0700

    Update ex5.py

--- exercises/ex5.py
+++ exercises/ex5.py
@@ -5,10 +5,13 @@ weight = 130 #lbs
 eyes = 'Brown'
 teeth = 'White'
 hair = 'Black'
+height_in = height * 0.393701 # study dril- do some math
+weight_kg = weight * 0.453592 # study drill- do some math
 
 print(f"Let's talk about {name}")
-print(f"He's {height} inches tall.")
-print(f"He's {weight} lbs heavy.")
+print(f"He's {height} inches ({height_in}) tall.") #study drill
+print(f"He's {weight} lbs ({weight_kg}) heavy.") #study drill
 print("Actually that's not too heavy.")
 print(f"He's got {eyes} eyes and {hair} hair.")
 print(f"His teeth are usually {teeth} depending on the coffee")
+
