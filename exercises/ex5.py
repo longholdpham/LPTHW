08f64f0d02dcd0ff76e64ee5a1d88bebc7c0f32d
longholdpham <longpham9903@gmail.com>
2022-09-24 20:06:39 -0700

    Create ex5.py

---
+++ exercises/ex5.py
@@ -0,0 +1,14 @@
+my_name = 'Long Pham'
+my_age = 99 
+my_height = 165 #cm
+my_weight = 130 #lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} lbs heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffe")
