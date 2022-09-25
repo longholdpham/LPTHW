04558541866ba49f5472c7e651f33992c7a1cc91
longholdpham <longpham9903@gmail.com>
2022-09-24 20:18:26 -0700

    Update ex5.py

--- exercises/ex5.py
+++ exercises/ex5.py
@@ -5,8 +5,8 @@ weight = 130 #lbs
 eyes = 'Brown'
 teeth = 'White'
 hair = 'Black'
-height_in = height * 0.40 # study dril- do some math
-weight_kg = weight * 0.45 # study drill- do some math
+height_in = round(height * 0.393701) # study dril- do some math
+weight_kg = round(weight * 0.453592) # study drill- do some math
 
 print(f"Let's talk about {name}")
 print(f"He's {height} cm ({height_in} inches) tall.") #study drill
