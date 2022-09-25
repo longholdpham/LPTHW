24adf880bd8d1961d0b86f332c2ce47751fb6e6a
longholdpham <longpham9903@gmail.com>
2022-09-24 18:26:37 -0700

    Changes to style

--- exercises/ex1.py
+++ exercises/ex1.py
@@ -1,8 +1,10 @@
-print ("Hello World!")
-print ("Hello Again")
-print ("I like typing this.")
-print ("This is fun.")
-print ("I'd much rather you 'not'.")
-print ('I "said" do not tough this.')
-print ("Printing another line.")
+# First Program
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("I'd much rather you 'not'.")
+print('I "said" do not tough this.')
+print("Printing another line.")
 # Comment
