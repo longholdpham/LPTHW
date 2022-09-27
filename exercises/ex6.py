1dcedafb0742ffb74da7fab2eff6384733924e25
longholdpham <longpham9903@gmail.com>
2022-09-26 21:00:14 -0700

    Update ex6.py

--- exercises/ex6.py
+++ exercises/ex6.py
@@ -1,18 +1,20 @@
-types_of_people = 10
-x = f"There are (types_of_people) types of people."
+types_of_people = 10 #define variable types_of_people
+x = f"There are {types_of_people} types of people." #set x as an f-string
 
-binary = "binary"
-do_not = "don't"
-y = f"Those who know {binary} and those who {do_not}."
+binary = "binary" #define variable binary
+do_not = "don't" #define variable do_not
+y = f"Those who know {binary} and those who {do_not}." 
+#set y as an f-string
 
-print(x)
-print(y)
+print(x) #print x variable
+print(y) #print y variable
 
-print(f"I said: {x}")
-print(f"I also said: '{y}'")
+print(f"I said: {x}") #print f-string with variable x
+print(f"I also said: '{y}'") #print f-string with variable y
 
-hilarious = False
-joke_evaluation = "Isn't that joke so funny?! {}"
+hilarious = False #define variable hilarious as a boolean
+joke_evaluation = "Isn't that joke so funny?! {}" 
+#define variable joke_evaluation
 
 print(joke_evaluation.format(hilarious))
 
@@ -20,3 +22,7 @@ w = "This is the left side off..."
 e = "a string with a right side."
 
 print(w + e) 
+
+###Test###
+
+
