38db4f16689a6cab29bfc88a2fd1bb64746e5aaa
longholdpham <longpham9903@gmail.com>
2022-09-29 13:27:44 -0700

    Create ex11.py

---
+++ exercises/ex11.py
@@ -0,0 +1,16 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()    
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+some_string1 = "Hello"
+some_string2 = "World"
+some_string3 = "!"
+
+print(f"{some_string1} {some_string2} {some_string3}.....")
+#testing print using formatter without needing to type 'format'. worked
+
