f84f872d7c6152a49ad44720c1216b819e66f862
longholdpham <longpham9903@gmail.com>
2022-09-29 19:30:22 -0700

    Update ex13.py

--- exercises/ex13.py
+++ exercises/ex13.py
@@ -1,14 +1,20 @@
 from sys import argv
 # read the WYSS section for how to run this
-script, first, second, third = argv
+script, first_variable, second_variable, third_variable = argv
 
-print("The script is calledL", script)
-print("Your first variable is:", first)
-print("Your second variable is:", second)
-print("Your third variable is:", third)
+#When running on the command prompt, the code will look like this when
+# typed in: python ex13.py first second third
+#                  script  first_variable second_variable third_variable
+
+print("The script is called:", script)
+print("Your first variable is:", first_variable)
+print("Your second variable is:", second_variable)
+print("Your third variable is:", third_variable)
 
 print(f"""The script is called {script}
 Your first variable is {first}
 Your second variable is {second}
 Your third variable is {third}
 """)
+# Testing printing + argv. worked
+# 
