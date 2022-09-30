164869f6b5b68f2edb01eb1f67e10d1cf76e6c18
longholdpham <longpham9903@gmail.com>
2022-09-30 10:28:12 -0700

    Update ex15.py

--- exercises/ex15.py
+++ exercises/ex15.py
@@ -2,9 +2,15 @@ from sys import argv
 
 script, filename = argv
 
-txt = open(filename)
+txt = open(filename) #open the file
 
-print(f "Here's your file {filename}:")
-print(txt.read())
+print(f"Here's your file {filename}:")
+print(" ")
+print(txt.read()) #print the content in the txt file
 
 print("Type the filename again:")
+file_again = input("> ") #prompting > 
+print(" ")
+txt_again = open(file_again)#open the file again
+
+print(txt_again.read())#print the content in the txt file again
