c09f205f627592c0f46bc6b6744fa3ffa3d1515f
longholdpham <longpham9903@gmail.com>
2022-09-29 19:23:00 -0700

    Update ex13.py

--- exercises/ex13.py
+++ exercises/ex13.py
@@ -6,3 +6,9 @@ print("The script is calledL", script)
 print("Your first variable is:", first)
 print("Your second variable is:", second)
 print("Your third variable is:", third)
+
+print(f"""The script is called {script}
+Your first variable is {first}
+Your second variable is {second}
+Your third variable is {third}
+""")
