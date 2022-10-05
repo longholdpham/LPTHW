7508d455b856c09e0a3db386aeb97c4c010a7ff4
longholdpham <longpham9903@gmail.com>
2022-10-04 22:08:08 -0700

    Update ex16 Reading and Writing Files.py

--- exercises/ex16 Reading and Writing Files.py
+++ exercises/ex16 Reading and Writing Files.py
@@ -1,6 +1,6 @@
 from sys import argv
 
-script, filename = argv
+script, filename = argv #unpacking argv
 
 print(f"We're going to erase {filename}.")
 print("If you don't want that, hit CTRL-C (^C).")
