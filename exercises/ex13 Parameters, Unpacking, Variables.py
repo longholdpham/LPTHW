74edcf41847884abae0070bb8afa7c692d3a6315
longholdpham <longpham9903@gmail.com>
2022-10-04 22:11:59 -0700

    Update ex13 Parameters, Unpacking, Variables.py

--- exercises/ex13 Parameters, Unpacking, Variables.py
+++ exercises/ex13 Parameters, Unpacking, Variables.py
@@ -1,10 +1,9 @@
 from sys import argv
 # read the WYSS section for how to run this
 script, first_variable, second_variable, third_variable = argv
-
-#When running on the command prompt, the code will look like this when
-# typed in: python ex13.py first second third
-#                  script  first_variable second_variable third_variable
+# "unpacking" argv variable
+# rather than holding all the arguments, it gets assigned to  four 
+# variables you can work with: script, first, second, and third.
 
 print("The script is called:", script)
 print("Your first variable is:", first_variable)
