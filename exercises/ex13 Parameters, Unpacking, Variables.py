7acf9ed98e77eeb4735bf3497740f3f4f1033ac2
longholdpham <longpham9903@gmail.com>
2022-10-04 22:24:49 -0700

    Update ex13 Parameters, Unpacking, Variables.py

--- exercises/ex13 Parameters, Unpacking, Variables.py
+++ exercises/ex13 Parameters, Unpacking, Variables.py
@@ -1,15 +1,16 @@
 from sys import argv
 # read the WYSS section for how to run this
-script, first_variable, second_variable, third_variable = argv
+script, first_variable, second_variable, third_variable, randomparameter = argv
 # "unpacking" argv variable
 # rather than holding all the arguments, it gets assigned to  four 
 # variables you can work with: script, first, second, and third.
 
+print(f"You passed a random parameter call {randomparameter}.")
 print("The script is called:", script)
 print("Your first variable is:", first_variable)
 print("Your second variable is:", second_variable)
 print("Your third variable is:", third_variable)
-fourth_variable = input("What is the fourth variable?")
+fourth_variable = input("What is the fourth variable? ")
 print("Your fourth variable is:", fourth_variable)
 
 
