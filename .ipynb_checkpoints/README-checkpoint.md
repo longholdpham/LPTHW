820b5ad5aec65aba91b968e9189cd40f499b5159
longholdpham <longpham9903@gmail.com>
2024-12-30 20:41:25 -0500

    we back

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,4 @@
+# Learn Python The Hard Way 5th edition
+A repo storing my exercises from the Learn Python The Hard Way 5th edition course by Zed Shaw
+
+https://learncodethehardway.com/client/#/product/learn-python-the-hard-way-5e-2023/
---
+++ .ipynb_checkpoints/exercise2-checkpoint.py
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your program later/
+# Anything after the # is ignored by python
+
+print("I could have code like this.") # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment ou code:
+# print("For example, this line won't run.")
+
+print("But this line will run.")
