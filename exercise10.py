809d60d7efed7a0d20284a8fb54732fecdaf06f8
longholdpham <longpham9903@gmail.com>
2025-01-16 21:36:17 -0500

    .

---
+++ exercise10.py
@@ -0,0 +1,16 @@
+tabby_cat = "\tI'm tabbed in." #\t = tab
+persian_cat = "I'm split\non a line." #\n = newline
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+\ryooo\r1\r2\r3 \"wud mean\"  
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
